534b5bbc7a2b85f8ab57160802a401db3791b89d
tsdnhts <jyjangtop@hanmail.net>
2024-02-14 13:11:17 +0900

    feat yml파일에 url,username,password 등 추가함

---
+++ src/main/kotlin/com/teamsparta/shoppingmalllist/ShoppingMallListApplication.kt
@@ -0,0 +1,11 @@
+package com.teamsparta.shoppingmalllist
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShoppingMallListApplication
+
+fun main(args: Array<String>) {
+	runApplication<ShoppingMallListApplication>(*args)
+}
---
+++ src/test/kotlin/com/teamsparta/shoppingmalllist/ShoppingMallListApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.teamsparta.shoppingmalllist
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShoppingMallListApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
