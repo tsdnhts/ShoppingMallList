1819b4ddd4856022e1cf11947eebde5587e00247
tsdnhts <jyjangtop@hanmail.net>
2024-02-20 21:40:34 +0900

    레포지토리 관련 내용 일단 // 처리하여 에러 문제 해결하여 재업로드

--- src/main/kotlin/com/teamsparta/shoppingmalllist/domain/shoppingmalllist/repository/StoreRepository.kt
+++ src/main/kotlin/com/teamsparta/shoppingmalllist/domain/shoppingmalllist/repository/StoreRepository.kt
@@ -7,8 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository
 import java.awt.print.Pageable
 
 interface StoreRepository : JpaRepository<Store, Long> {
-    fun findAll(pageable: Pageable): Page<Store>?
-    fun findByScoreAndState(score : Int, state : StoreState, pageable: Pageable) : Page<Store>
+//    fun findAll(pageable: Pageable): Page<Store>?
+//    fun findByScoreAndState(score : Int, state : StoreState, pageable: Pageable) : Page<Store>
 }
 //interface ReviewRepository : JpaRepository<Review?, Long?> {
 //    fun findAll(pageable: Pageable?): Page<Review?>?
